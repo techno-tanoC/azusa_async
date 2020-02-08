eb8404cb0f176c06fdb53ac0023285947a1dd9fd
techno-tanoC <develop.tanoc@gmail.com>
2020-02-08 21:43:15 +0900

    add progress

---
+++ src/progress.rs
@@ -0,0 +1,27 @@
+use serde::Serialize;
+
+#[derive(Serialize, Debug, Clone, PartialEq, Eq)]
+pub struct Progress {
+    name: String,
+    total: u64,
+    size: u64,
+    canceled: bool
+}
+
+impl Progress {
+    pub fn new(name: String, total: u64, size: u64) -> Self {
+        Progress { name, total, size, canceled: false }
+    }
+
+    pub fn progress(&mut self, size: u64) {
+        self.size += size;
+    }
+
+    pub fn set_total(&mut self, total: u64) {
+        self.total = total;
+    }
+
+    pub fn cancel(&mut self) {
+        self.canceled = true;
+    }
+}
