69ef4681db5085057efd6df939e4196f2f19f5c3
techno-tanoC <develop.tanoc@gmail.com>
2020-02-10 23:31:19 +0900

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+mod app;
+mod download;
+mod lock_copy;
+mod progress;
+mod table;
+
+use tokio::runtime::Runtime;
+
 fn main() {
+    let mut rt = Runtime::new().unwrap();
+    let app = app::App::new();
+    let download = download::Download::new();
+
     println!("Hello, world!");
+
+    rt.block_on(async {
+        download.start(&app, url, &".", name, ext).await;
+    });
+
+    println!("See you, world!");
 }
