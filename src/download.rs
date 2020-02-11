9016845a923c50ac6487e4a16d4981d441a80856
techno-tanoC <develop.tanoc@gmail.com>
2020-02-11 18:41:21 +0900

    update download

--- src/download.rs
+++ src/download.rs
@@ -14,14 +14,7 @@ impl Download {
         Download(reqwest::Client::new())
     }
 
-    pub async fn start<P: AsRef<Path>>(
-        &self,
-        app: &App,
-        url: &str,
-        dest: &P,
-        name: &str,
-        ext: &str,
-    ) {
+    pub async fn start<P: AsRef<Path>>(&self, app: &App, url: &str, dest: &P, name: &str, ext: &str) {
         let (temp, path) = tempfile::NamedTempFile::new().unwrap().into_parts();
         let mut f: File = temp.into();
         let mut res = self.0.get(url).send().await.unwrap();
@@ -34,18 +27,26 @@ impl Download {
                 app.table.set_total(&id, cl).await;
             }
 
-            Self::read_chunks(app, &id, &mut res, &mut f).await;
+            let flag = Self::read_chunks(app, &id, &mut res, &mut f).await;
 
-            app.lock_copy.copy(&path, dest, name, ext).await;
-            app.table.delete(&id).await;
+            if flag {
+                app.lock_copy.copy(&path, dest, name, ext).await;
+                app.table.delete(&id).await;
+            }
         }
     }
 
-    async fn read_chunks(app: &App, id: &str, res: &mut reqwest::Response, f: &mut File) {
+    async fn read_chunks(app: &App, id: &str, res: &mut reqwest::Response, f: &mut File) -> bool {
         while let Some(byte) = res.chunk().await.unwrap() {
-            app.table.progress(id, byte.as_ref().len() as u64).await;
-            f.write_all(&byte).await.unwrap();
+            if app.table.is_canceled(id).await.unwrap() {
+                return false;
+            } else {
+                app.table.progress(id, byte.as_ref().len() as u64).await;
+                f.write_all(&byte).await.unwrap();
+                println!("{:?}", app.table.to_vec().await)
+            }
         }
+        return true;
     }
 
     fn content_length(res: &Response) -> Option<u64> {
