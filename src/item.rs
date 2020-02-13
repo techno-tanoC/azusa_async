fc7d07b032fe58bfdde6d5033eaf5e3690e7f0d9
techno-tanoC <develop.tanoc@gmail.com>
2020-02-13 15:00:25 +0900

    add item

---
+++ src/item.rs
@@ -0,0 +1,24 @@
+use serde::Serialize;
+
+use super::progress::Progress;
+
+#[derive(Serialize, Debug, Clone, PartialEq, Eq)]
+pub struct Item {
+    pub id: String,
+    pub name: String,
+    pub total: u64,
+    pub size: u64,
+    pub canceled: bool,
+}
+
+impl Item {
+    pub fn from_progress(id: String, pg: Progress) -> Item {
+        Item {
+            id,
+            name: pg.name,
+            total: pg.total,
+            size: pg.size,
+            canceled: pg.canceled,
+        }
+    }
+}
