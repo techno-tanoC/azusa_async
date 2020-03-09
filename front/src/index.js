d499f737009619accca4adb9a5bb7ce92085e93a
techno-tanoC <develop.tanoc@gmail.com>
2020-03-09 19:46:07 +0900

    setup react

--- front/src/index.js
+++ front/src/index.js
@@ -1,1 +1,10 @@
-console.log("hello world!")
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <h1>Hello world!</h1>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
