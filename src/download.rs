0c7093e915c2c68c07742acaa4499270c88cc351
techno-tanoC <develop.tanoc@gmail.com>
2020-03-11 01:05:43 +0900

    use read_stream

--- src/download.rs
+++ src/download.rs
@@ -1,7 +1,9 @@
+use bytes::Bytes;
 use reqwest::{header, Response};
 use std::path::*;
 use tokio::fs::File;
 use tokio::prelude::*;
+use tokio::stream::{Stream, StreamExt};
 
 use super::app::App;
 use super::progress::Progress;
@@ -21,7 +23,7 @@ impl Download {
 
     pub async fn start<P: AsRef<Path>>(&self, app: &App, url: &str, dest: &P, name: &str, ext: &str) -> Result<()> {
         let mut temp: File = tempfile::tempfile()?.into();
-        let mut res = self.0.get(url).send().await?;
+        let res = self.0.get(url).send().await?;
 
         debug!("{:?}", &res);
 
@@ -31,7 +33,7 @@ impl Download {
             app.table.add(&id, pg).await;
 
             info!("name: {:?}, url: {:?}, id: {:?}", &name, url, &id);
-            let ret = Self::download(app, &id, &mut res, &mut temp, dest, name, ext).await;
+            let ret = Self::download(app, &id, res, &mut temp, dest, name, ext).await;
 
             app.table.delete(&id).await;
             ret
@@ -40,12 +42,13 @@ impl Download {
         }
     }
 
-    async fn download<P: AsRef<Path>>(app: &App, id: &str, res: &mut reqwest::Response, temp: &mut File, dest: &P, name: &str, ext: &str) -> Result<()> {
+    async fn download<P: AsRef<Path>>(app: &App, id: &str, res: reqwest::Response, temp: &mut File, dest: &P, name: &str, ext: &str) -> Result<()> {
         if let Some(cl) = Self::content_length(&res) {
             app.table.set_total(&id, cl).await;
         }
 
-        let flag = Self::read_chunks(app, &id, res, temp).await?;
+        let mut stream = res.bytes_stream();
+        let flag = Self::read_stream(app, &id, &mut stream, temp).await?;
 
         if flag {
             app.lock_copy.copy(temp, dest, name, ext).await?;
@@ -54,16 +57,20 @@ impl Download {
         Ok(())
     }
 
-    async fn read_chunks<W: AsyncWrite + Unpin>(app: &App, id: &str, res: &mut reqwest::Response, f: &mut W) -> Result<bool> {
-        while let Some(byte) = res.chunk().await? {
+    async fn read_stream<W, S>(app: &App, id: &str, stream: &mut S, f: &mut W) -> Result<bool>
+        where
+            W: AsyncWrite + Unpin,
+            S: Stream<Item = reqwest::Result<Bytes>> + Unpin
+    {
+        while let Some(bytes) = stream.next().await.transpose()? {
             match app.table.is_canceled(id).await {
                 Some(true) => {
                     debug!("canceled id: {:?}", id);
                     return Ok(false);
                 },
                 Some(false) => {
-                    app.table.progress(id, byte.as_ref().len() as u64).await;
-                    f.write_all(&byte).await?;
+                    app.table.progress(id, bytes.as_ref().len() as u64).await;
+                    f.write_all(&bytes).await?;
                 },
                 None => {
                     error!("read_chunks id not found in table: id {:?}", id);
